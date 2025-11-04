0f420e647f70293a3abc1dc8864cdb4b05c222dc
fuyu <88916226+fuyu28@users.noreply.github.com>
2025-11-05 01:25:46 +0900

    fix:zodのoptionalがnullを認めずエラーが出るのを修正

--- src/lib/content/frontmatterSchema.ts
+++ src/lib/content/frontmatterSchema.ts
@@ -4,7 +4,7 @@ export const FrontmatterSchema = z.object({
   // 記事タイトル
   title: z.string(),
   // サムネイル画像URL
-  thumbnail: z.string().optional(),
+  thumbnail: z.string().nullish(),
   // 記事タイプ (技術 / 趣味 / 雑記)
   type: z.enum(["tech", "hobby", "misc"]),
   // 公開状態 (公開 / 非公開)
@@ -14,13 +14,13 @@ export const FrontmatterSchema = z.object({
   // Deep状態可否
   isDeep: z.boolean().default(false),
   // 公開日
-  publishedAt: z.coerce.date().optional(),
+  publishedAt: z.coerce.date().nullish(),
   // 更新日
-  updatedAt: z.coerce.date().optional(),
+  updatedAt: z.coerce.date().nullish(),
   // 説明文
-  description: z.string().optional(),
+  description: z.string().nullish(),
   // タグ
-  topics: z.array(z.string()).optional(),
+  topics: z.array(z.string()).nullish(),
 });
 
 export type Frontmatter = z.infer<typeof FrontmatterSchema>;
